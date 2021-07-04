2c397df488413e18dfff15656c234fb61f6f2105
Raj Kishore Sahni <rajkishoresahni826@gmail.com>
2021-07-04 17:28:31 +0530

    use case 5

--- src/components/payroll_form/payroll_form.jsx
+++ src/components/payroll_form/payroll_form.jsx
@@ -229,22 +229,66 @@ const PayrollForm = (props) => {
                         <label htmlFor="startDate"  className="label-text" >Start Date</label>
                         <div>
                             <select name="day" id="day" onChange={changeValue} >
-                                <option value="1">1</option>
-                                <option value="2">2</option>
+                                
+                                <option value="01">1</option>
+                                <option value="02">2</option>
+                                <option value="03">3</option>
+                                <option value="04">4</option>
+                                <option value="05">5</option>
+                                <option value="06">6</option>
+                                <option value="07">7</option>
+                                <option value="08">8</option>
+                                <option value="09">9</option>
+                                <option value="10">10</option>
+                                <option value="11">11</option>
+                                <option value="12">12</option>
+                                <option value="13">13</option>
+                                <option value="14">14</option>
+                                <option value="15">15</option>
+                                <option value="16">16</option>
+                                <option value="17">17</option>
+                                <option value="18">18</option>
+                                <option value="19">19</option>
+                                <option value="20">20</option>
+                                <option value="21">21</option>
+                                <option value="22">22</option>
+                                <option value="23">23</option>
+                                <option value="24">24</option>
+                                <option value="25">25</option>
+                                <option value="26">26</option>
+                                <option value="27">27</option>
+                                <option value="28">28</option>
+                                <option value="29">29</option>
+                                <option value="30">30</option>
+                                <option value="31">31</option>
                             </select>
                             <select name="month" id="month" onChange={changeValue}>
                                 <option value="jan">January</option>
                                 <option value="feb">Febuary</option>
+                                <option value="Mar">March</option>
+                                <option value="Apr">April</option>
+                                <option value="May">May</option>
+                                <option value="Jun">June</option>
+                                <option value="July">July</option>
+                                <option value="Aug">August</option>
+                                <option value="Sep">September</option>
+                                <option value="Oct">October</option>
+                                <option value="Nov">November</option>
+                                <option value="Dec">December</option>
                             </select>
-                            <select name="yaer" id="year" onChange={changeValue}>
+                            <select name="year" id="year" onChange={changeValue}>
                                 <option value="2020">2020</option>
+                                <option value="2019">2019</option>
+                                <option value="2018">2018</option>
+                                <option value="2017">2017</option>
+                                <option value="2016">2016</option>
                             </select>
                         </div>
                     </div>
                     <div className="error" >{formValue.error.startDate}</div>
 
                     <div className="row">
-                        <label htmlFor="notex" className="label text">Notes</label>
+                        <label htmlFor="notes" className="label text">Notes</label>
                         <textarea onChange={changeValue} id="notes" value={formValue.notes} className="input" placeholder="" style={{height:'100%'}}></textarea>
                     </div>
                     <div className="buttonParent">
--- src/services/axios-service.js
+++ src/services/axios-service.js
@@ -5,6 +5,18 @@ class AxiosService {
       return axios.post(url, payload, tokenRequired && httpOptions);
     }
 
+    putService(url = '', payload = null, tokenRequired = false, httpOptions = null) {
+        return axios.put(url, payload, tokenRequired && httpOptions);
+      }
+    
+    deleteService(url = '', tokenRequired = false, httpOptions = null) {
+        return axios.delete(url, tokenRequired && httpOptions);
+    }
+    
+    getService(url = '', tokenRequired = false, httpOptions = null) {
+        return axios.get(url, tokenRequired && httpOptions);
+    }
+
 }
 module.exports = new AxiosService();
 
