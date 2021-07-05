14b11fa84ccd040b76133bc420bdc98c519e8e69
Raj Kishore Sahni <rajkishoresahni826@gmail.com>
2021-07-06 02:01:59 +0530

    combined uc7 and uc6

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import PayrollForm from './components/payroll-form/payroll-form';
+import HomePage from './components/home/employee-home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route, Redirect
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path = "/home"><HomePage /></Route>
+          <Route exact path = "/payroll-form"><PayrollForm /></Route>
+          <Route exact path="/payroll-form/:id"><PayrollForm /></Route>
+          <Route exact path=""><Redirect exact from="/" to="/home" /></Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/services/employee-service.js
@@ -0,0 +1,25 @@
+import config from '../config/config';
+import AxiosService from './axios-service';
+
+// const URL = config.baseUrl + "employeepayrollservice";
+const URL = config.baseUrl;
+
+
+export default class EmployeeService {
+
+  addEmployee(employeeData) {
+    return AxiosService.postService(`${URL}/employee-payroll`, employeeData);
+  }
+  getAllEmployees() {
+    return AxiosService.getService(`${URL}/employee-payroll`);
+  }
+  getEmployeeById(id) {
+    return AxiosService.getService(`${URL}/employee-payroll${id}`);
+  }
+  updateEmployee(data) {
+    return AxiosService.putService(`${URL}/employee-payroll/${data.id}`, data);
+  }
+  deleteEmployee(id) {
+    return AxiosService.deleteService(`${URL}/employee-payroll/${id}`);
+  }
+}
